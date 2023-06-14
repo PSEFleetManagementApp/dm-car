02c8d37d9f3c46939b3fb7188941da490e01b915
I556063 <tim.lachenicht@sap.com>
2023-06-14 22:31:51 +0200

    add dtos, mapper and main

--- DM-Car/src/main.go
+++ DM-Car/src/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"car/DM-Car/src/api/controller"
+	"car/DM-Car/src/infrastructure"
+	"car/DM-Car/src/logic/operations"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	carOperations := operations.NewCarOperations(infrastructure.NewCarRepository())
+	carCollectionResource := controller.NewCarCollectionResource(carOperations)
+
+	http.HandleFunc("/car", carCollectionResource.HandleAddCar)
+	http.HandleFunc("/car/", carCollectionResource.HandleGetCar)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatalf("System failed to listen and serve car collection resource: %v", err)
+	}
 }
