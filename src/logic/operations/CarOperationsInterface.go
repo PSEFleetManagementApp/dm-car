419dad6006222486ef4906023d123e2bcbc6a826
Niklas Sänger <niklas.saenger@kit.edu>
2023-09-20 16:55:41 +0200

    refactor

--- src/logic/operations/CarOperationsInterface.go
+++ src/logic/operations/CarOperationsInterface.go
@@ -1,11 +1,11 @@
 package operations
 
 import (
-	model2 "car/logic/model"
+	"car/logic/model"
 )
 
 type CarOperationsInterface interface {
-	AddCar(vin string, brand string, model string) (model2.Car, error)
-	GetCars() (model2.Cars, error)
-	GetCar(vin string) (model2.Car, error)
+	AddCar(vin string, brand string, model string) (model.Car, error)
+	GetCars() (model.Cars, error)
+	GetCar(vin string) (model.Car, error)
 }
